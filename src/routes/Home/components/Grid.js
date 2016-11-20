c37da1de5ee22ab5f9ea27695996b1a1d428e856
treeforever <treeforever7@gmail.com>
2016-11-20 00:54:54 -0500

    set up annyang voice recognition

--- src/routes/Home/components/Grid.js
+++ src/routes/Home/components/Grid.js
@@ -7,7 +7,7 @@ import StarBorder from 'material-ui/svg-icons/toggle/star-border'
 const styles = {
   root: {
     display: 'flex',
-    'flex-direction': 'row',
+    flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-around'
   },
@@ -58,7 +58,6 @@ const Grid = () => (
           key={tile.img}
           title={tile.title}
           subtitle={<span>by <b>{tile.author}</b></span>}
-          actionIcon={<IconButton><StarBorder color='white' /></IconButton>}
         >
           <img src={tile.img} />
         </GridTile>
